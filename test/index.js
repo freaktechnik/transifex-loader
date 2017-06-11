e57b03862cc1bfa89f9920f76f19a0b2555908fd
Martin Giger <martin@humanoids.be>
2017-06-11 20:10:15 +0200

    Gracefully handle network errors

--- test/index.js
+++ test/index.js
@@ -88,3 +88,4 @@ test("found resource and wrote it back to disk", async (t) => {
 
 test.todo("No-caching query");
 test.todo("Fallback to .transifexrc in home dir");
+test.todo('Network error when fetching resource content');
